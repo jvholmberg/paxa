2124a6c8a8787fd5759b665f825f1c4bf7de28c2
Johan Holmberg <johan@forkyfork.com>
2022-01-02 00:42:26 +0100

    Moving to ng-zorro due to licensing-reasons

--- Paxa/ClientApp/src/app/shared/shared.module.ts
+++ Paxa/ClientApp/src/app/shared/shared.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatCardModule } from '@angular/material/card';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+
+import { IconsProviderModule } from '../icons-provider.module';
 
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { DialogsModule } from '@progress/kendo-angular-dialog';
@@ -29,26 +21,17 @@ import { NavigationModule } from '@progress/kendo-angular-navigation';
 
 import { LoginComponent } from './components/login/login.component';
 
-
-
 const externalLibs = [
   CommonModule,
   FontAwesomeModule,
+  FormsModule,
   ReactiveFormsModule,
-  MatButtonModule,
-  MatDialogModule,
-  MatIconModule,
-  MatInputModule,
-  MatSidenavModule,
-  MatSelectModule,
-  MatSnackBarModule,
-  MatTableModule,
-  MatToolbarModule,
-  MatProgressBarModule,
-  MatCardModule,
-  MatListModule,
-  MatGridListModule,
-  MatFormFieldModule,
+
+  NzLayoutModule,
+  NzMenuModule,
+  NzBreadCrumbModule,
+
+  IconsProviderModule,
 
   ButtonsModule,
   DialogsModule,
--- Paxa/ClientApp/src/app/welcome/welcome.component.spec.ts
+++ Paxa/ClientApp/src/app/welcome/welcome.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LandingComponent } from './landing.component';
+import { WelcomeComponent } from './welcome.component';
 
-describe('LandingComponent', () => {
-  let component: LandingComponent;
-  let fixture: ComponentFixture<LandingComponent>;
+describe('WelcomeComponent', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
+      declarations: [ WelcomeComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LandingComponent);
+    fixture = TestBed.createComponent(WelcomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
