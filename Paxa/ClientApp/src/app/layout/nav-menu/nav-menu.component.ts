54fb84dce23d6385d3823b5743c867a52a04381b
Johan Holmberg <johan@forkyfork.com>
2022-01-01 01:35:53 +0100

    Appbar fixed somewhat

--- Paxa/ClientApp/src/app/layout/nav-menu/nav-menu.component.ts
+++ Paxa/ClientApp/src/app/layout/nav-menu/nav-menu.component.ts
@@ -5,10 +5,12 @@ import { UserService } from '@user/services/user.service';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.css'],
 })
 export class NavMenuComponent {
 
+  public kendokaAvatar =
+    "https://www.telerik.com/kendo-angular-ui-develop/components/navigation/appbar/assets/kendoka-angular.png";
   isExpanded = false;
   isLoading$ = this.httpsStatusService.loading$;
   isLoggedIn$ = this.userService.loggedIn$;
