9937734951604bc73032282ffbcb7d16d8df40a4
Johan Holmberg <johan@forkyfork.com>
2022-01-04 23:19:47 +0100

    Changed all styles to css

--- Paxa/ClientApp/src/app/get-started/get-started.component.ts
+++ Paxa/ClientApp/src/app/get-started/get-started.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-get-started',
   templateUrl: './get-started.component.html',
-  styleUrls: ['./get-started.component.scss']
+  styleUrls: ['./get-started.component.css']
 })
 export class GetStartedComponent implements OnInit {
 
--- Paxa/ClientApp/src/app/not-allowed/not-allowed.component.ts
+++ Paxa/ClientApp/src/app/not-allowed/not-allowed.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-allowed',
   templateUrl: './not-allowed.component.html',
-  styleUrls: ['./not-allowed.component.scss']
+  styleUrls: ['./not-allowed.component.css']
 })
 export class NotAllowedComponent implements OnInit {
 
