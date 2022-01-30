8307b11f3dd50d30a6804c710e386caf766c02b8
Johan Holmberg <johan@forkyfork.com>
2022-01-30 13:52:48 +0100

    Added project to sln for running background-services

---
+++ Paxa.Scheduled/Program.cs
@@ -0,0 +1,11 @@
+using Paxa.Scheduled;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
