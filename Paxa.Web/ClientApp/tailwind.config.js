390918ff493bfe5e1f0aced4baa01cc8e5150723
Johan Holmberg <johan@forkyfork.com>
2022-03-23 00:21:03 +0100

    ...

--- Paxa.Web/ClientApp/tailwind.config.js
+++ Paxa.Web/ClientApp/tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
       colors: {
         brand: {
           primary: "#019fe3",
-          accent: "#e9148c"
+          primaryLight: "#51b3e9",
+          primaryDark: "#0480b5",
+          accent: "#e9148c",
+          accentLight: "#f051a3",
+          accentDark: "#ba1470",
         }
       }
     },
